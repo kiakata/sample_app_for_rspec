e97beeb6c31955f3a82066ec1fe07d775355c843
Takaaki Uno <takaaki.uno@gmail.com>
2019-11-14 07:14:38 +0900

    add factorybotのtasks.rbでテスト用のtaskを作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    title { 'rspec test' }
+    content { 'test content' }
+    status { 0 }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,44 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   let(:user) { create(:user) }
+  let(:task) { build(:task, user: user) }
+
   context 'with valid attributes' do
     it "is valid with title, text and user_id" do
-      task = user.tasks.build(
-      title: 'rspec test',
-      content: 'test content ...',
-      status: 0
-      )
       expect(task).to be_valid
     end
   end
+
   context 'with invalid attributes' do
     it 'is invalid without title' do
-      task = user.tasks.build(
-      content: 'test content ...',
-      status: 0
-      )
+      task.title = nil
       expect(task).to be_invalid
       expect(task.errors[:title]).to include("can't be blank")
     end
+
     it 'is invalid without status' do
-      task = user.tasks.build(
-      title: 'rspec test',
-      content: 'test content ...',
-      )
+      task.status = nil
       expect(task).to be_invalid
       expect(task.errors[:status]).to include("can't be blank")
     end
+
     it 'is invalid with not unique title' do
-      task1 = user.tasks.create(
-      title: 'same title',
+      task.save
+      task_2 = user.tasks.build(
+      title: task.title,
       status: 0
       )
-      task2 = user.tasks.build(
-      title: 'same title',
-      status: 0
-      )
-      expect(task2).to be_invalid
-      expect(task2.errors[:title]).to include('has already been taken')
+      expect(task_2).to be_invalid
+      expect(task_2.errors[:title]).to include('has already been taken')
     end
   end
 end
