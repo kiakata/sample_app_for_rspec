fc16276aa58c95687e9d8d615b6835faca7dfb83
Takaaki Uno <takaaki.uno@gmail.com>
2019-11-14 21:28:43 +0900

    modify 変数名を修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe Task, type: :model do
 
     it 'is invalid with not unique title' do
       task.save
-      task_2 = task.user.tasks.build(
+      same_title_task = task.user.tasks.build(
         title: task.title
       )
-      expect(task_2).to be_invalid
-      expect(task_2.errors[:title]).to include('has already been taken')
+      expect(same_title_task).to be_invalid
+      expect(same_title_task.errors[:title]).to include('has already been taken')
     end
   end
 end
